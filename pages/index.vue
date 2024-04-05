0a75f9d234b3f0e3654cbec35997c155bf4f5d20
ryan echternacht <ryan@echternacht.org>
2024-04-05 16:56:32 -0400

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>i'm the root</div>
+</template>
+
+<script setup>
+// works fine
+useServerSeoMeta({
+  title: 'redirect page',
+  ogTitle: 'redirect page',
+  description: 'redirect description',
+  ogDescription: 'redirect description',
+  // ogImage: ,
+})
+</script>
---
+++ pages/one.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>i'm not a real page, just a redirect</div>
+</template>
+
+<script setup>
+// gets thrown away cuz of the redirect
+useServerSeoMeta({
+  title: 'redirect page',
+  ogTitle: 'redirect page',
+  description: 'redirect description',
+  ogDescription: 'redirect description',
+  // ogImage: ,
+})
+
+await navigateTo('/')
+</script>
